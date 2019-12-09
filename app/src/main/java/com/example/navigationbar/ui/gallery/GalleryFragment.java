bcfec87e8ad09caf1dcdc99a8c356fc942f1ea04
bihwal <strength147@gmail.com>
2019-12-09 11:58:54 +0545

    ALMOST COMPLETED

--- app/src/main/java/com/example/navigationbar/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/navigationbar/ui/gallery/GalleryFragment.java
@@ -17,7 +17,7 @@ import com.example.navigationbar.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class GalleryFragment extends Fragment implements View.OnClickListener {
+public class GalleryFragment extends Fragment  {
 
     EditText etprincple, ettime,etrate;
     Button btnSI;
@@ -28,41 +28,28 @@ public class GalleryFragment extends Fragment implements View.OnClickListener {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_gallery, container, false);
+
         etprincple = view.findViewById(R.id.etprincple);
-        ettime=view.findViewById(R.id.etnareacirc);
+        ettime=view.findViewById(R.id.ettime);
         etrate=view.findViewById(R.id.etrate);
         btnSI = view.findViewById(R.id.btnSI);
         tvdisplaya=view.findViewById(R.id.tvresult);
+
+    btnSI.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            float principle,rate,time,result;
+            principle=Float.parseFloat(etprincple.getText().toString());
+            rate=Float.parseFloat(etrate.getText().toString());
+            time=Float.parseFloat(ettime.getText().toString());
+
+            result=(principle*rate*time)/100;
+
+            tvdisplaya.setText("Simple Interest is : " + result);
+        }
+    });
         return view;
-    }
-
-    @Override
-    public void onClick(View v) {
-
-        if (TextUtils.isEmpty(etprincple.getText()))
-        {
-            etprincple.setError("Enter Principle ");
-            return;
-        }
-
-        if (TextUtils.isEmpty(etrate.getText()))
-        {
-            etrate.setError("Enter Rate");
-            return;
-        }
-        if (TextUtils.isEmpty(ettime.getText()))
-        {
-            ettime.setError("Enter Time");
-            return;
-        }
-        float principle,rate,time,result;
-        principle=Float.parseFloat(etprincple.getText().toString());
-        rate=Float.parseFloat(etrate.getText().toString());
-        time=Float.parseFloat(ettime.getText().toString());
-
-        result=(principle*rate*time)/100;
-
-        tvdisplaya.setText(Float.toString(result));
 
     }
+
 }
